4fb35f1b79046792719429145f3c2380582bede8
Kravtsiv Yura <kravtsivyura@gmail.com>
2018-05-13 14:48:21 +0300

    ajax

---
+++ js/widget.js
@@ -0,0 +1,72 @@
+
+	var employees = [
+	  {
+	   "name": "Aimee",
+	   "inoffice": false
+	  },
+	  {
+	   "name": "Amit",
+	   "inoffice": false
+	  },
+	  {
+	   "name": "Andrew",
+	   "inoffice": true
+	  },
+	  {
+	   "name": "Ben",
+	   "inoffice": true
+	  },
+	  {
+	   "name": "Elizabeth",
+	   "inoffice": true
+	  },
+	  {
+	   "name": "Guil",
+	   "inoffice": false
+	  },
+	  {
+	   "name": "Hampton",
+	   "inoffice": true
+	  },
+	  {
+	   "name": "Jason",
+	   "inoffice": true
+	  },
+	  {
+	   "name": "Lainie",
+	   "inoffice": true
+	  },
+	  {
+	   "name": "Kenneth",
+	   "inoffice": false
+	  },
+	  {
+	   "name": "Pasan",
+	   "inoffice": true
+	  },
+	  {
+	   "name": "Shawna",
+	   "inoffice": true
+	  },
+	  {
+	   "name": "Zac",
+	   "inoffice": false
+	  }
+	];
+	$('ul li').addClass('out');
+
+	// var selectedLi = $('ul li');
+	// 		for(var i = 0; i < employees.length; i++){
+	// 			if(employees[i].inoffice == true){
+	// 				console.log(selectedLi.get(i));
+	// 				selectedLi.get(i).addClass('in');
+	// 			// } else if(employees[i].inoffice == false){
+	// 			// 	console.log(i++);
+	// 			// 	$('.bulleted li').eq(i).addClass('out');
+	// 			}
+	// 		}
+
+
+
+
+
